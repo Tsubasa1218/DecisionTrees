e7b53d4380f5fb10b0bc2072dff9f9a7e1be739e
Tsubasa1218 <juan.suaza1218@gmail.com>
2017-11-26 00:12:42 -0500

    Finish main process of obtaining info g from data TODO: Define the loop for making the chil nodes

--- MCClassificationAlgorithm/MCClassificationAlgorithm.py
+++ MCClassificationAlgorithm/MCClassificationAlgorithm.py
@@ -1,7 +1,35 @@
 #This project is made to compare an implementation of a classifier algorithm for Machine Learning
-
-import math
 import numpy as np
+
+data_set_attributes = [
+        [140, 'Smooth', 'Red'],
+        [130, 'Smooth', 'Red'], 
+        [150, 'Bumpy', 'Yellow'], 
+        [170, 'Bumpy', 'Yellow'], 
+        [177, 'Bumpy', 'Yellow'], 
+        [135, 'Smooth', 'Red'], 
+        [140, 'Smooth', 'Red'],
+        [144, 'Bumpy', 'Yellow'],
+        ]
+data_set_labels = ['Apple', 'Apple', 'Orange', 'Orange', 'Orange', 'Apple', 'Apple', 'Orange']
+np.random.seed(21)
+for i in range(0, 10):
+    size = np.random.randint(130, 180)
+    rndText = np.random.randint(0, 2)
+    if rndText == 0:
+        text = 'Smooth'
+    else:
+        text = 'Bumpy'
+    rndColor = np.random.randint(0, 2)
+    if rndColor == 0:
+        color = 'Red'
+        obs = 'Apple'
+    else:
+        color = 'Yellow'
+        obs = 'Orange'
+    data_set_attributes.append([size, text, color])
+    data_set_labels.append(obs)
+
 
 class Node:
     def __init__(self):
@@ -12,25 +40,102 @@ class Node:
         self.value = None
         
         #Leaves
-        self.left = None
-        self.right = None
+        self.childs_values = []
+        self.childs = []
 
 def Classification():
-    data_set_attributes = [[140, 'Smooth'], [130, 'Smooth'], [150, 'Bumpy'], [170, 'Bumpy'], [177, 'Bumpy'], [135, 'Smooth'], [140, 'Smooth']]
-    data_set_labels = ['Apple', 'Apple', 'Orange', 'Orange', 'Orange', 'Apple']
+    
+    #print(data_set_attributes)
+    #print(data_set_labels)
 
     #We have to discretize the values that are continous
-    print(data_set_attributes)
     cleaned_data = Discretizer(data_set_attributes)
 
+    #We get the labels from the data
+    label_list = LabelsList(data_set_labels)
+    #We calculate the frecuencis
+    labels_frequency = Frecuency(data_set_labels, label_list)
+    #We calculate the entropy of the system. That is the entropy of the labels
+    entropy_of_the_system = Entropy(labels_frequency)
+    #print(entropy_of_the_system)
+    
+    #Now we can start setting up the actual process of finding the nodes
+    #Define the root node
     tree = Node()
     tree.value = cleaned_data
 
-    label_groups, labels_list = Preprocessing(tree)
+    attr_groups, attr_list = Preprocessing(tree)
+    
+    #Calculate the info. g. of all colums
+    entropies = ColumnEntropies(tree.value, data_set_labels, label_list, attr_groups)
 
-    Frecuency(tree, labels_list)
+    #Find the infoG = EntropyOfSystem - ColumEntropies
+    info_gainance = [0]*len(entropies)
+    for i in range(0, len(info_gainance)):
+        info_gainance[i] = entropy_of_the_system - entropies[i]
 
-    #info_gainance_of_system = 
+    max_info_g = max(info_gainance)
+    max_info_pos = info_gainance.index(max_info_g)
+
+    #We have to remove the column from the cleaned_data array/list
+    cleaned_data_cp = cleaned_data.copy()
+    cleaned_data.clear()
+    
+    for i in range(0, len(cleaned_data_cp)):
+        aux_list = []
+        for j in range(0, len(cleaned_data_cp[i])):
+            if j != max_info_pos:
+                aux_list.append(cleaned_data_cp[i][j])
+        cleaned_data.append(aux_list)
+    
+
+    #Create of the new nodes
+    #for i in range(0, attr_groups[max_info_pos]):
+    #    child = Node()
+    #    child.parent = tree  
+    #    child.value = cleaned_data
+
+    #    tree.childs.append(child)
+    #    tree.childs_values.append(attr_groups[max_info_pos][i])
+        
+    
+
+    
+
+
+
+def ColumnEntropies(value_data, label_data, label_list, attr_groups):
+     
+    info_gainance = [0]*len(attr_groups)
+    #This fors calculate the frecuency of an item given the label forr every attribute
+    #I.e. how many less than avg are apples and oranges
+    #It also calculates the entropy of each column (attribute). Then it calculates the info gainance
+    for i in range(0, len(attr_groups)):
+        for j in range(0, len(attr_groups[i])):
+            #print("se evalua ", attr_groups[i][j])
+            frequency = [0]*len(attr_groups[i])
+            for k in range(0, len(value_data)):
+                if value_data[k][i] == attr_groups[i][j]:
+                    for h in range(0, len(label_list)):
+                        if label_list[h] == label_data[k]:
+                            frequency[h] += 1
+            
+            #Here we calculate the frequency for each attr value
+            column = GetColumnOfMatrix(value_data, i)
+            attr_frequency = Frecuency(column, attr_groups[i])
+            ocur = attr_frequency[j]
+            total = sum(attr_frequency)
+
+            #Here we calculate the info gainance of each attr. The formula is: (ocur/total)*Entropy(frequencies)
+            info_gainance[i] += ProbabilityFunction(ocur, total) * Entropy(frequency)
+    
+    return info_gainance
+
+def GetColumnOfMatrix(value_data, column_pos):
+    list = [0]*len(value_data)
+    for value in value_data:
+        list.append(value[column_pos])
+    return list
 
 def Discretizer(data):
     type_names = []
@@ -71,9 +176,16 @@ def Discretizer(data):
         
     return data
 
+def LabelsList(data):
+    labels = []
+    for label in data:
+        if(label not in labels):
+            labels.append(label)
+
+    return labels
 
 def Preprocessing(tree):
-    #This part tries to define the set of labels that are going to be evaluated 
+    #This part tries to define the set of attributes that are going to be evaluated 
     #Only for this case, where we are evaluating only the second column <- don't pay attention
 
     #TODO: Scale this code to be used in N columns. DONE!!!
@@ -99,19 +211,17 @@ def Preprocessing(tree):
 
     return label_groups, labels_list
 
-def Frecuency(tree, labels_list):
+def Frecuency(values_list, labels_list):
     #This section is used to count how many characteristics are in the list
     #To proceed to calculate Entropy values for each characteristic
     label_frequency_list = [0]*len(labels_list)
     
-    for element in tree.value:
-        for i in range(0, len(labels_list)):
-            for j in range(0, len(element)):
-                if(element[j] == labels_list[i]):
-                    label_frequency_list[i] += 1
-    print(label_frequency_list)
-    
-        
+    for i in range(0, len(values_list)):
+        for j in range(0, len(labels_list)):
+            if labels_list[j] == values_list[i]:
+                label_frequency_list[j] += 1
+    return label_frequency_list
+            
 def Entropy(list):
     #This function calculates the entropy value using:
     #E(x)=-SUM( P(x)*Log2(P(x)) ) ; P is the probability function
@@ -119,11 +229,16 @@ def Entropy(list):
     entropy_value = 0
     total_frequency = 0
     
-    for i in list:
-        total_frequency += i
+    for value in list:
+        total_frequency += value
     
     for i in list:
-        entropy_value += ProbabilityFunction(i, total_frequency) * math.log2(ProbabilityFunction(i, total_frequency))
+        a = ProbabilityFunction(i, total_frequency)
+        if a > 0:
+            b =np.log2(a)
+        else:
+            b = 0.0
+        entropy_value +=  a*b 
     return entropy_value * -1
             
 
